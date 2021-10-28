107c86b6142677862bb902c620ce5ee11ba20a63
mehdi@yib.io <mehdi@yib.io>
2021-10-28 02:54:01 +0100

    finishing the app after midnight

---
+++ model/User.js
@@ -0,0 +1,28 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize("alg_auth", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+(async () => {
+  await sequelize.sync();
+})();
+
+class User extends Model {}
+User.init(
+  {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    password: DataTypes.STRING,
+    issuing_auth: DataTypes.STRING,
+    release_date: DataTypes.STRING,
+    expire_date: DataTypes.STRING,
+    name: DataTypes.STRING,
+    birthday: DataTypes.STRING,
+    blood_type: DataTypes.STRING,
+    sex: DataTypes.STRING,
+    place_of_birth: DataTypes.STRING,
+  },
+  { sequelize, modelName: "user" }
+);
+
+module.exports = { User, sequelize };
